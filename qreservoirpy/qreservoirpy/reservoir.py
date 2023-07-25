3c18fd6103ff7821f8195230e1d9305b71dcf28f
otkulseng <otkulseng1@gmail.com>
2023-07-25 10:04:49 +0200

    moved incrementally to reservoir

--- qreservoirpy/qreservoirpy/reservoir.py
+++ qreservoirpy/qreservoirpy/reservoir.py
@@ -8,77 +8,68 @@ from tqdm import tqdm
 
 
 class QReservoir:
-    def __init__(self, qubits, layers, analyze_function=lambda res:res, **kwargs) -> None:
+    def __init__(self, qubits, layers, analyze_function=lambda res:res, incrementally=False, M=np.inf, **kwargs) -> None:
         self.qreg = QuantumRegister(qubits, name='q')
         self.layers = layers
 
-        self.total_runned = 0
-        self.states = []
+        self.incrementally = incrementally
+        self.M = M
 
         self.kwargs = kwargs
         self.analyze_fcn = analyze_function
 
-        self.n_features = self.analyze_fcn(np.zeros(2*len(self.qreg)))
 
     def run(self, timeseries, shots=10000, transpile=False, simulator='aer_simulator_statevector'):
-        print("Started running")
-        self.total_runned += len(timeseries)
+        len_timeseries = len(timeseries)
 
+        if self.incrementally:
+            timeseries = [
+                timeseries[:i+1][-self.M:] for i in range(len_timeseries)
+            ]
+        else:
+            timeseries = [timeseries]
 
-
+        result = []
         with tqdm(total=len(timeseries)) as pbar:
-            pbar.set_postfix(running="Setup", refresh=False)
-            while len(timeseries) > 0:
-                Nold = len(timeseries)
-                timeseries = self.add_timeseries(timeseries)
-
-
-                pbar.set_postfix(running="Building", refresh=True)
-                circ = self.__build()
-                pbar.set_postfix(running="Simulating", refresh=True)
+            pbar.set_description("Simulating")
+            for series in timeseries:
+                circ = self.__build(series)
 
                 mem = utilities.simulate(circ, shots, transpile, simulator)
-                self.states.append(self.analyze_fcn(utilities.memory_to_mean(mem, 1)))
 
-                Nnew = len(timeseries)
-                pbar.update(Nold-Nnew)
+                result.append(self.analyze_fcn(utilities.memory_to_mean(mem, 1)))
+
+                pbar.update(1)
 
 
-        return np.array(self.states).reshape((self.total_runned, -1))
-
-    def add_timeseries(self, timeseries):
-        for layer in self.layers:
-            timeseries = layer.add_timeseries(timeseries)
-        return timeseries
+        return np.array(result).reshape((len_timeseries, -1))
 
     @property
     def circuit(self):
-        return self.__build()
+        return self.__build([0, 1, 0, 1])
 
-    # @property
-    # def states(self):
-    #     return np.array(self.states)
 
-    def __get_num_measurements(self):
+    def __get_num_measurements(self, series):
         num_meas = 0
         for layer in self.layers:
-            num_meas += layer.get_num_measurements(self.qreg)
+            num_meas += layer.get_num_measurements(self.qreg, series)
         return num_meas
 
-    def __build(self):
+
+    def __build(self, series):
         ## There are probably more efficient ways of doing this
-        num_meas = self.__get_num_measurements()
+        num_meas = self.__get_num_measurements(series)
         creg = ClassicalRegister(num_meas)
         circ = ReservoirCircuit(self.qreg, creg)
 
         for layer in self.layers:
-            circ = layer.build(circ, **self.kwargs)
+            circ = layer.build(circ, series, **self.kwargs)
 
         self.creq = ClassicalRegister(circ.RC_measured, name='c')
         newCirc = ReservoirCircuit(self.qreg, self.creq)
 
         for layer in self.layers:
-            newCirc = layer.build(newCirc, **self.kwargs)
+            newCirc = layer.build(newCirc, series, **self.kwargs)
         return newCirc
 
 
