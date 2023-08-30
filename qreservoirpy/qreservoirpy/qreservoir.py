bb625bb47f7250dacbabd517fcf387010b3a9256
otkulseng <otkulseng1@gmail.com>
2023-08-30 16:16:46 +0200

    Working, but user still need to specify fairly advanced analyze fcn

--- qreservoirpy/qreservoirpy/qreservoir.py
+++ qreservoirpy/qreservoirpy/qreservoir.py
@@ -36,8 +36,9 @@ class CountingCircuit(InterfaceCircuit):
         self.count += num_meas
         return instr
 
+from abc import ABC, abstractmethod
 
-class BaseReservoir:
+class BaseReservoir(ABC):
     """
     Interface for custom reservoirs. Please note that one should NOT
     inherit from this class directly, but rather through the subclass
@@ -55,56 +56,48 @@ class BaseReservoir:
     def after(self, circuit):
         pass
 
+    @abstractmethod
+    def analyze_fcn(self, meas):
+        pass
+
 
 class QReservoir(BaseReservoir):
-    def __init__(self, n_qubits, backend=Aer.get_backend('aer_simulator')) -> None:
+    def __init__(self, n_qubits, backend=Aer.get_backend('aer_simulator'), memory=np.inf) -> None:
         if not issubclass(type(backend), Backend):
             raise TypeError("Argument 'backend' must inherit from qiskits abstract 'Backend'")
 
         self.n_qubits = n_qubits
         self.backend = backend
+        self.memory = memory
 
 
-    def run(self, timeseries, analyze=True, memory=True, **kwargs):
+    def run(self, timeseries, **kwargs):
         circ = self.circuit(timeseries, merge_registers=False).reverse_bits()
-        self._job = self.backend.run(circ, memory=memory, **kwargs)
-
-        if not analyze:
-            return
-
+        self._job = self.backend.run(circ, memory=True, **kwargs)
         mem = self._job.result().get_memory()
         avg = memory_to_mean(mem)
 
-        idx = len(avg) - self.post_meas
-        during_data = avg[:idx]
-        after_data = avg[idx:].reshape((1, -1))
+        return self.analyze_fcn(avg)
 
-        if during_data.size == 0:
-            return after_data
 
-        during_data = during_data.reshape((len(timeseries), -1))
-        if after_data.size == 0:
-            return during_data
+    def predict(self, num_pred, model, from_series, **kwargs):
+        M = min(num_pred + len(from_series), self.memory)
 
-        return during_data, after_data
-
-    def predict(self, num_pred, model, from_series, analyze_fcn=lambda v : v[-1], **kwargs):
         predictions = np.zeros(num_pred + len(from_series))
         predictions[:len(from_series)] = from_series
 
         for i in tqdm(range(num_pred), desc="Predicting..."):
             curidx = len(from_series) + i
-            states = self.run(predictions[:curidx], kwargs=kwargs)
-            pred_state = analyze_fcn(states).reshape((1, -1))
+            states = self.run(predictions[:curidx][-M:], kwargs=kwargs)
+            pred_state = states[-1].reshape((1, -1))
             predictions[curidx] = model.predict(pred_state)
 
         return predictions[-num_pred:]
 
 
+    def run_incrementally(self, timeseries, analyze=True, **kwargs):
 
-    def run_incrementally(self, timeseries, M=np.inf, analyze=True, **kwargs):
-
-        M = min(len(timeseries), M)
+        M = min(len(timeseries), self.memory)
         timeseries_splited = [timeseries[:i+1][-M:] for i in range(len(timeseries))]
 
         total = 0
@@ -123,17 +116,20 @@ class QReservoir(BaseReservoir):
 
 
             result = self._job.result()
-            num_features = memory_to_mean(result.get_memory(0)).size
+            num_features = self.analyze_fcn(memory_to_mean(result.get_memory(0))).size
             states = np.zeros((len(timeseries_splited), num_features))
 
 
             pbar.set_description("Analyzing... ")
             for idx, _ in enumerate(timeseries_splited):
                 memory = self._job.result().get_memory(idx)
-                states[idx] = memory_to_mean(memory)
+                states[idx] = self.analyze_fcn(memory_to_mean(memory))
                 pbar.update(1)
 
+            self.num_features = states.shape[-1]
             return states
+
+
 
 
     def circuit(self, timeseries, merge_registers=False):
@@ -165,53 +161,4 @@ class QReservoir(BaseReservoir):
 
         self.post_meas = after - before
         return circ
-
-
-
-encoding = {
-    0: '00',
-    1: '01',
-    2: '10',
-    3: '11'
-}
-
-from qiskit.quantum_info import random_unitary
-class BRes(QReservoir):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.op = random_unitary(2 ** self.n_qubits)
-
-    def before(self, circuit):
-        for qbit in circuit.qubits:
-            circuit.initialize('+', qbit)
-
-    def during(self, circuit, timestep):
-        circuit.barrier()
-        for i in range(2):
-            circuit.initialize('+', 0)
-            circuit.cx(0, i+1)
-            # circuit.measure([0, 1])
-        circuit.append(self.op, circuit.qubits)
-        circuit.initialize(encoding[timestep], [0, 1])
-
-    def after(self, circuit):
-        circuit.measure_all()
-
-
-
-
-
-from qiskit_aer import AerSimulator
-res = BRes(n_qubits=4, backend=AerSimulator())
-
-
-# res.circuit([0, 1, 2] * 4, merge_registers=True).draw('mpl')
-
-states = res.run_incrementally([0, 1, 2]*10, M=20, shots=100)
-
-# print(states)
-# import matplotlib.pyplot as plt
-# res.__build([0, 1, 2, 3]).draw('mpl')
-
-plt.show()
 
