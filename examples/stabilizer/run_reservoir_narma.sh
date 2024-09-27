9c186f39c8397a6d77dcafabf69ec78d6a09fb92
ScoomenstheMumens <95868499+ScoomenstheMumens@users.noreply.github.com>
2024-09-27 14:02:18 +0200

    Add files via upload

--- examples/stabilizer/run_reservoir_narma.sh
+++ examples/stabilizer/run_reservoir_narma.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=random_sample
 #              d-hh:mm:ss
 #SBATCH --time=30-00:00:00
-#SBATCH --output=/home/rubenb/quantumreservoirpy_vivaldi/%j.out
+#SBATCH --output=/home/rubenp/quantumreservoirpy_vivaldi/%j.out
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=1
