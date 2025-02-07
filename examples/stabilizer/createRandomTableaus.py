ec1ba5527e658b20335e1418d71159ef03a14228
Franz Fuchs <franzgeorgfuchs@gmail.com>
2025-02-07 15:20:46 +0100

    added creation of random tableaus

---
+++ examples/stabilizer/createRandomTableaus.py
@@ -0,0 +1,13 @@
+from quantumreservoirpy.stabilizer import Stabilizer
+import pickle
+
+def createanddumptableaus(num_qubits, num_measurements):
+    num_tableaus = 100
+    tableaus = {}
+    for nr in range(1, num_tableaus+ 1):
+        tableaus[nr] = Stabilizer.generate_tableau(num_qubits, num_measurements)
+    with open("tableau_"+"num_qubits"+str(num_qubits)+"_num_measurements"+str(num_measurements)+"_num_tableaus"+str(num_tableaus)+".pickle","wb") as f:
+        pickle.dump(tableaus, f)
+
+createanddumptableaus(5,3)
+
