45d71e8fba08440e187c7e2c1ebcd82a4f4a2a76
otkulseng <otkulseng1@gmail.com>
2025-06-08 13:04:30 +0200

    tests and doi fix

---
+++ tests/test_imports.py
@@ -0,0 +1,7 @@
+def test_import_main():
+    import quantumreservoirpy
+
+def test_import_submodules():
+    from quantumreservoirpy import music
+    from quantumreservoirpy import reservoirs
+    from quantumreservoirpy import plot
